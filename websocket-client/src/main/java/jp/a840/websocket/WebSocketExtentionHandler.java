92a50a5662b181656989ec820f650993de3df80d
hashio <mlg28671@gmail.com>
2011-04-30 21:03:59 +0900

    move websocket-socket to https://github.com/hashio/websocket-client

--- websocket-client/src/main/java/jp/a840/websocket/WebSocketExtentionHandler.java
+++
@@ -1,7 +0,0 @@
-package jp.a840.websocket;
-
-import jp.a840.websocket.handler.StreamHandler;
-
-public interface WebSocketExtentionHandler extends StreamHandler {
-
-}
--- websocket-client/src/main/java/jp/a840/websocket/frame/FrameHeader.java
+++
@@ -1,9 +0,0 @@
-package jp.a840.websocket.frame;
-
-import java.nio.ByteBuffer;
-
-public interface FrameHeader {
-	public long getFrameLength();
-	public long getBodyLength();
-	public ByteBuffer toByteBuffer();
-}
